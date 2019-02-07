dae15bb401d2362847776589a82e769fcb2bad5c
Ty Barho <t.barho@gmail.com>
2019-02-07 10:03:31 -0600

    split out vis reg

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,22 @@
+const isVisualRegression = !!(process.env.VIZ_REG);
+
+const testPathIgnorePatterns = isVisualRegression ? ['node_modules'] : [
+  '/node_modules/',
+  '/stories.spec.tsx',
+  '/stories.spec.js',
+];
+
+const testRegex = isVisualRegression ? '(/__tests__/.*|(.|/)(stories.)(test|spec)).js?$' : '(/__tests__/.*|(.|/)(test|spec)).js?$';
+
 module.exports = {
-  testMatch: ['**/*.spec.js'],
-  testPathIgnorePatterns: process.env.STORYBOOK ? [] : ['stories.spec.js'],
+  roots: ['<rootDir>/apps'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testRegex,
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
   reporters: ['jest-spec-reporter', 'jest-junit'],
   globalSetup: '<rootDir>test/globalSetup.js',
   setupTestFrameworkScriptFile: '<rootDir>test/testSetup.js',
+  testPathIgnorePatterns,
 };
